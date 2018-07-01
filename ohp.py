d341010e0007308fb7e757bb13be28341e3284f7
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-07-01 21:31:43 +0430

    add simple entry point

---
+++ ohp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# __check_external_modules created to check requirements before load the engine
+if __name__ == "__main__":  # and __check_external_modules():
+    pass  # load()  # load and parse the ARGV
