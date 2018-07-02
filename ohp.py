b8558d2a79dce2cad6aeb0f233efbab6282829ec
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-07-02 14:57:50 +0430

    simple first load

--- ohp.py
+++ ohp.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from core.get_modules import load_all_modules
+from core.alert import info
+from core.color import finish
+from core.alert import messages
+from core.compatible import logo
 
 # __check_external_modules created to check requirements before load the engine
 if __name__ == "__main__":  # and __check_external_modules():
-    pass  # load()  # load and parse the ARGV
+    logo()
+    info(messages("en", "honeypot_started"))
+    info(messages("en", "available_modules"))
+    for module in load_all_modules():
+        info(module)
+    finish()
