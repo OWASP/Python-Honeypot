1900dd511b98d213b3d7e5db1eed729f67b0122b
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-07-04 15:22:47 +0430

    add user configuration

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from core.compatible import hours
+
 
 def docker_configuration():
     """
@@ -11,6 +13,20 @@ def docker_configuration():
     """
     return {
         "virtual_machine_storage_limit": 0.5,  # Gigabyte
-        "virtual_machine_container_reset_factory_time": 1 * 60 * 60  # hours
+        "virtual_machine_container_reset_factory_time": hours(1),  # hours
 
+    }
+
+
+def user_configuration():
+    """
+        user configuration
+
+    Returns:
+        JSON/Dict user configuration
+    """
+    return {
+        "language": "en",
+        "default_selected_modules": "ftp/weak_password,ssh/weak_password,http/weak_password",
+        "default_excluded_modules": "",
     }
