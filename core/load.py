33cf11dee2377fabd005570f382d2aab5218126a
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-07-04 15:23:08 +0430

    minor update

--- core/load.py
+++ core/load.py
@@ -6,7 +6,38 @@ from core.alert import info
 from core.color import finish
 from core.alert import messages
 from core.compatible import logo
+from core.compatible import version
+from core.compatible import os_name
+from config import user_configuration
 
+# temporary use fixed version of argparse
+if os_name() == "win32" or os_name() == "win64":
+    if version() is 2:
+        from lib.argparse.v2 import argparse
+    else:
+        from lib.argparse.v3 import argparse
+else:
+    import argparse
+
+
+def argv_parser():
+    parser = argparse.ArgumentParser(prog="Nettacker", add_help=False)
+    engineOpt = parser.add_argument_group(messages("en", "engine"), messages("en", "engine_input"))
+    engineOpt.add_argument("-m", "--select-module", action="store",
+                           dest="selected_modules", default=user_configuration()["default_selected_modules"],
+                           help=messages("en", "select_module").format(load_all_modules()))
+    engineOpt.add_argument("-x", "--exclude-module", action="store",
+                           dest="selected_modules", default=user_configuration()["default_excluded_modules"],
+                           help=messages("en", "exclude_module").format(load_all_modules()))
+    engineOpt.add_argument("-s", "--vm-storage-limit", action="store",
+                           dest="virtual_machine_storage_limit", type=float,
+                           default=user_configuration()["virtual_machine_storage_limit"],
+                           help=messages("en", "vm_storage_limit"))
+    engineOpt.add_argument("-r", "--vm-reset-factory-time", action="store",
+                           dest="virtual_machine_storage_limit", type=float,
+                           default=user_configuration()["virtual_machine_container_reset_factory_time"],
+                           help=messages("en", "vm_reset_factory_time"))
+    
 
 def load_honeypot_engine():
     logo()
--- lib/language/messages_en.py
+++ lib/language/messages_en.py
@@ -14,5 +14,13 @@ def all_messages():
             "honeypot_started": "OWASP Honeypot started ...",
             "available_modules": "list of available modules",
             "module_not_available": "module {0} is not available",
-            "docker_error": "cannot communicate with docker, please install and start the docker service!"
+            "docker_error": "cannot communicate with docker, please install and start the docker service!",
+            "engine": "OHP Engine",
+            "engine_input": "OHP Engine input options",
+            "select_module": "select module(s) {0}",
+            "exclude_module": "select modules(s) to exclude {0}",
+            "vm_storage_limit": "virtual machine storage limit",
+            "vm_reset_factory_time": "virtual machine reset factory time",
+
+
         }
