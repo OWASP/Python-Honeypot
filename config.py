8c7c7a952e64faa84fe287ff032a17a27473378e
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-07-15 05:31:12 +0430

    add API and Database configuration

--- config.py
+++ config.py
@@ -2,6 +2,32 @@
 # -*- coding: utf-8 -*-
 
 from core._time import hours
+import random
+
+
+def api_configuration():
+    """
+    API Config (could be modify by user)
+
+    Returns:
+        a JSON with API configuration
+    """
+    return {  # OWASP Honeypot API Default Configuration
+        "api_host": "127.0.0.1",
+        "api_port": 5000,
+        "api_debug_mode": False,
+        "api_access_key": "".join(random.choice("0123456789abcdef") for x in range(32)),
+        "api_client_white_list": {
+            "enabled": False,
+            "ips": ["127.0.0.1", "10.0.0.0/24", "192.168.1.1-192.168.1.255"]
+        },
+        "api_access_log": {
+            "enabled": False,
+            "filename": "ohp_api_access.log"
+        },
+        "api_database": "mongodb://127.0.0.1:27017/",  # mongodb://user:password@127.0.0.1:27017/
+        "database_name": "ohp_events"
+    }
 
 
 def docker_configuration():
