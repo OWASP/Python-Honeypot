a5949ae9daed2d36a30695c68e83b4b32963758c
Ali Razmjoo <ali.razmjoo@owasp.org>
2018-07-24 16:46:44 +0430

    apply ip2location in database + code clean

--- database/connector.py
+++ database/connector.py
@@ -2,15 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import pymongo
+import os
+import inspect
 
 from core._time import now
 from config import api_configuration
+from lib.ip2location import IP2Location
 
-client = pymongo.MongoClient(api_configuration()["api_database"], serverSelectionTimeoutMS=api_configuration()[
-    "api_database_connection_timeout"])
+client = pymongo.MongoClient(
+    api_configuration()["api_database"],
+    serverSelectionTimeoutMS=api_configuration()["api_database_connection_timeout"]
+)
 database = client[api_configuration()["api_database_name"]]
 honeypot_events = database.honeypot_events
 network_events = database.network_events
+IP2Location = IP2Location.IP2Location(
+    os.path.join(
+        os.path.dirname(
+            inspect.getfile(IP2Location)
+        ),
+        "IP2LOCATION-LITE-DB1.BIN")
+)
 
 
 def insert_selected_modules_network_event(ip, port, module_name):
@@ -25,8 +37,15 @@ def insert_selected_modules_network_event(ip, port, module_name):
     Returns:
         ObjectId(inserted_id)
     """
-    return honeypot_events.insert_one({"ip": ip, "port": int(port),
-                                  "module_name": module_name, "date": now()}).inserted_id
+    return honeypot_events.insert_one(
+        {
+            "ip": ip,
+            "port": int(port),
+            "module_name": module_name,
+            "date": now(),
+            "country": IP2Location.get_country_short(ip)
+        }
+    ).inserted_id
 
 
 def insert_other_network_event(ip, port):
@@ -40,4 +59,11 @@ def insert_other_network_event(ip, port):
     Returns:
         ObjectId(inserted_id)
     """
-    return network_events.insert_one({"ip": ip, "port": int(port), "date": now()}).inserted_id
+    return network_events.insert_one(
+        {
+            "ip": ip,
+            "port": int(port),
+            "date": now(),
+            "country": IP2Location.get_country_short(ip)
+        }
+    ).inserted_id
