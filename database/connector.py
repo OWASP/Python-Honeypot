09d3f1359cf8f84491d1dd2b4491822e10ee5e61
dhirensr <dhirensr@gmail.com>
2019-07-12 17:53:28 +0200

    mongodb bulk adding code
    what : after every 1minute the events will get inserted in bulk and when pressen
    ctrl+c the remaining events will get inserted in bulk

--- database/connector.py
+++ database/connector.py
@@ -4,6 +4,7 @@
 import pymongo
 import os
 import inspect
+import time
 
 from core._time import now
 from config import api_configuration
@@ -16,6 +17,9 @@ client = pymongo.MongoClient(
 database = client[api_configuration()["api_database_name"]]
 honeypot_events = database.honeypot_events
 network_events = database.network_events
+global honeypot_events_queue,network_events_queue
+honeypot_events_queue=[]
+network_events_queue=[]
 IP2Location = IP2Location.IP2Location(
     os.path.join(
         os.path.dirname(
@@ -40,7 +44,8 @@ def insert_selected_modules_network_event(ip, port, module_name, machine_name):
     Returns:
         ObjectId(inserted_id)
     """
-    return honeypot_events.insert_one(
+    global honeypot_events_queue
+    honeypot_events_queue.append(
         {
             "ip": ip,
             "port": int(port),
@@ -49,7 +54,8 @@ def insert_selected_modules_network_event(ip, port, module_name, machine_name):
             "machine_name": machine_name,
             "country": str(IP2Location.get_country_short(ip).decode())
         }
-    ).inserted_id
+    )
+    return
 
 
 def insert_other_network_event(ip, port, machine_name):
@@ -64,7 +70,8 @@ def insert_other_network_event(ip, port, machine_name):
     Returns:
         ObjectId(inserted_id)
     """
-    return network_events.insert_one(
+    global network_events_queue
+    network_events_queue.append(
         {
             "ip": ip,
             "port": int(port),
@@ -72,4 +79,30 @@ def insert_other_network_event(ip, port, machine_name):
             "machine_name": machine_name,
             "country": str(IP2Location.get_country_short(ip).decode())
         }
-    ).inserted_id
+    )
+    return
+
+
+def insert_events_in_bulk():
+    """
+    inserts all honeypot and network events in bulk to honeypot_events and network_events collection respectively
+    """
+    global honeypot_events_queue
+    global network_events_queue
+    if honeypot_events_queue:
+        honeypot_events.insert_many(honeypot_events_queue)
+    if network_events_queue:
+        network_events.insert_many(network_events_queue)
+    honeypot_events_queue=[]
+    network_events_queue=[]
+    return
+
+
+def insert_bulk_events_from_thread():
+    '''
+    Thread function for inserting bulk events in a thread
+    '''
+    while True:
+        insert_events_in_bulk()
+        time.sleep(60)
+    return True
