05151452497f73711c7490a34abeaf9fe42f8b65
Ali Razmjoo <ali.razmjoo@owasp.org>
2019-08-14 21:23:09 +0200

    clean code http basic auth strong password

--- lib/modules/http/basic_auth_strong_password/__init__.py
+++ lib/modules/http/basic_auth_strong_password/__init__.py
@@ -6,13 +6,15 @@ from core.compatible import generate_token
 import os
 import binascii
 import json
-from database.connector import insert_honeypot_events_from_module_processor,\
-    insert_honeypot_events_data_from_module_processor
+from database.connector import insert_honeypot_events_from_module_processor
+from database.connector import insert_honeypot_events_data_from_module_processor
+
 
 class ModuleProcessor:
     """
     this is the processor to run after docker machine is up to grab the log files or do other needed process...
     """
+
     def __init__(self):
         self.log_filename = 'tmp/access.log'
         self.log_filename_dump = 'tmp/ohp_http_strong_password_creds_logs.json'
@@ -28,19 +30,26 @@ class ModuleProcessor:
                 # os.rename(self.log_filename, self.log_filename_dump)
                 data_dump = open(self.log_filename).readlines()
                 open(self.log_filename, 'w').write('')
-                #data_dump = open(self.log_filename_dump).readlines()
+                # data_dump = open(self.log_filename_dump).readlines()
                 for data in data_dump:
-                    data_json=json.loads(data)
-                    ip= data_json["ip"]
-                    time_of_insertion=data_json["time"]
-                    request= data_json['request']
-                    user_agent=data_json['user_agent']
+                    data_json = json.loads(data)
+                    ip = data_json["ip"]
+                    time_of_insertion = data_json["time"]
+                    request = data_json['request']
+                    user_agent = data_json['user_agent']
                     if data_json["authorization"] != "-":
-                        authorization=data_json["authorization"].split(' ')[1]
-                        authorization= binascii.a2b_base64(authorization).decode('utf-8') #binascii is returning bytes
-                        username,password=authorization.split(":")
-                        insert_honeypot_events_from_module_processor(ip,username,password,\
-                                                                     "http/basic_auth_strong_password", time_of_insertion)
+                        authorization = data_json["authorization"].split(' ')[1]
+                        authorization = binascii.a2b_base64(
+                            authorization
+                        ).decode('utf-8')  # binascii is returning bytes
+                        username, password = authorization.split(":")
+                        insert_honeypot_events_from_module_processor(
+                            ip,
+                            username,
+                            password,
+                            "http/basic_auth_strong_password",
+                            time_of_insertion
+                        )
             time.sleep(0.1)
 
 
