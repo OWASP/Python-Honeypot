38e1ab2320e8df47d52854d6869247aead16b765
dhirensr <dhirensr@gmail.com>
2019-08-20 13:04:07 +0200

    made changes for ftp weak strategy for docker host

--- lib/modules/ftp/weak_password/__init__.py
+++ lib/modules/ftp/weak_password/__init__.py
@@ -4,7 +4,47 @@
 import time
 from core.compatible import generate_token
 import os
+import json
+import datetime
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
+EXCLUDES = ['/dev']
+LOGFILE='tmp/ohp_ftp_weak_password_logs.txt'
+
+class Watcher:
+
+    def __init__(self):
+        self.observer = Observer()
+        self.stop_execution=False
+        self.DIRECTORY_TO_WATCH = os.getcwd() + "/tmp/ohp_ftp_weak_container/"
+
+    def run(self):
+        event_handler = Handler()
+        self.observer.schedule(event_handler, self.DIRECTORY_TO_WATCH, recursive=True)
+        self.observer.start()
+        try:
+            while not self.stop_execution:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print("Error")
+        self.observer.join()
+
+
+class Handler(FileSystemEventHandler):
+
+    @staticmethod
+    def on_any_event(event):
+        if not (event.event_type == 'modified' and event.is_directory) and '/' + event.src_path.rsplit('/')[1] not in EXCLUDES:
+            logfile_handle = open(LOGFILE, "a")
+            print(event.event_type,event.src_path)
+            logfile_handle.write(json.dumps({'status' : event.event_type,
+                        'path': event.src_path,
+                        'module_name' : 'ftp/weak_password',
+                        'date' : datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")})
+                                 +'\n')
+            logfile_handle.close()
 
 class ModuleProcessor:
     """
@@ -12,13 +52,19 @@ class ModuleProcessor:
     """
     def __init__(self):
         self.kill_flag = False
+        self.log_filename='tmp/ohp_ftp_weak_password_logs.txt'
+        self.watcher = Watcher()
 
     def processor(self):
         """
         processor function will be called as a new thread and will be die when kill_flag is True
         :return:
         """
+        if os.path.exists(self.log_filename):
+            os.remove(self.log_filename)  # remove if exist from past
         while not self.kill_flag:
+            #self.watcher.run() # when this is done it transfers the control to run and never returns back
+            # maybe one solution would be to run this in a different thread
             time.sleep(0.1)
 
 
@@ -32,6 +78,6 @@ def module_configuration():
     return {
          "username": "admin",
          "password": "admin",
-         "extra_docker_options": ["--volume {0}/tmp:/root/logs/".format(os.getcwd())],
+         "extra_docker_options": ["--volume {0}/tmp/ohp_ftp_weak_container/:/root".format(os.getcwd())],
          "module_processor": ModuleProcessor()
     }
