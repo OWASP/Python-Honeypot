d6f00863a64a479e43c67766e32dec9d2ea68177
mzfr <Mehtab.zafar98@gmail.com>
2020-02-24 19:00:34 +0530

    Properly format the code

--- tests/test_api_utilities.py
+++ tests/test_api_utilities.py
@@ -1,23 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import unittest
 
-from api.utility import fix_date, fix_limit, fix_skip, msg_structure
+from api.utility import fix_date
+from api.utility import fix_limit
+from api.utility import fix_skip
+from api.utility import msg_structure
 
 
 class TestApiUtility(unittest.TestCase):
 
     def test_msg_structure_pass(self):
-        expected_output = {"status": "ok",
-                           "msg": "Hello OWASP"
-                           }
-        returned_values = msg_structure("ok", "Hello OWASP")
-        self.assertEqual(returned_values, expected_output)
+        self.assertEqual(
+            msg_structure("ok", "Hello OWASP"),
+            {
+                "status": "ok",
+                "msg": "Hello OWASP"
+            }
+        )
 
     def test_msg_structure_fail(self):
-        expected_output = {"status": "ok",
-                           "msg": "Hello again"
-                           }
-        returned_values = msg_structure("failed", "Hello again")
-        self.assertNotEqual(returned_values, expected_output)
+        self.assertNotEqual(
+            msg_structure("failed", "Hello again"),
+            {
+                "status": "ok",
+                "msg": "Hello again"
+            }
+        )
 
     def test_fix_skip_pass(self):
         returned_values = fix_skip('0')
@@ -40,21 +49,19 @@ class TestApiUtility(unittest.TestCase):
         self.assertFalse(isinstance(returned_values, str))
 
     def test_fix_date(self):
-        returned_values = fix_date("02:05:2019")
-        expected_output = ['02:05:2019', '02:05:2019 23:59:59']
-        self.assertListEqual(expected_output, returned_values)
-
-    def test_fix_date_2(self):
-        returned_values = fix_date("02:05:2019 | 01:05:2019")
-        expected_output = ['02:05:2019 ', ' 01:05:2019']
-        self.assertListEqual(expected_output, returned_values)
-
-    def test_fix_date_3(self):
-        returned_values = fix_date("02/05/2019 | 01/05/2019")
-        expected_output = ['02/05/2019  00:00:00', ' 01/05/2019 23:59:59']
-        self.assertListEqual(expected_output, returned_values)
-
-    def test_fix_date_4(self):
-        returned_values = fix_date("02/05/2019")
-        expected_output = ['02/05/2019 00:00:00', '02/05/2019 23:59:59']
-        self.assertListEqual(expected_output, returned_values)
+        self.assertListEqual(
+            fix_date("02:05:2019"),
+            ['02:05:2019', '02:05:2019 23:59:59']
+        )
+        self.assertListEqual(
+            fix_date("02:05:2019 | 01:05:2019"),
+            ['02:05:2019 ', ' 01:05:2019']
+        )
+        self.assertListEqual(
+            fix_date("02/05/2019 | 01/05/2019"),
+            ['02/05/2019  00:00:00', ' 01/05/2019 23:59:59']
+        )
+        self.assertListEqual(
+            fix_date("02/05/2019"),
+            ['02/05/2019 00:00:00', '02/05/2019 23:59:59']
+        )
