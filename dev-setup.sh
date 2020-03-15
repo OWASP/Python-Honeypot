cfd0fae359ce6b7e8f117d9fa15eb83c083e9751
Chakshu <c.gupta@student.utwente.nl>
2020-03-15 15:43:14 +0100

    Update the development setup script

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,6 +1,11 @@
+##
+## Setup script for Ubuntu 18.04 Development system
+##
+
 pip install -r requirements.txt
 pip install -r requirements-dev.txt
 
+# Install MongoDB
 response=$(wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -)
 
 if ["$response" == "OK"]; then
@@ -24,3 +29,27 @@ sudo systemctl start mongod
 sudo systemctl enable mongod
 sudo systemctl stop mongod
 sudo systemctl restart mongod
+
+# Install tshark
+
+sudo apt install -y libgcrypt11-dev
+sudo apt install bison flex
+sudo apt install qttools5-dev-tools qttools5-dev libqt5svg5-dev qtmultimedia5-dev
+
+# Install Optional Dependencies
+sudo apt install libssh-dev asciidoctor libxml2-*
+sudo apt install doxygen libpcap-dev gnutls* krb5-*
+sudo apt install libsnappy1v5 libsnappy-dev libsnappy-java libsnappy-jni
+sudo apt install libmaxminddb-dev libbrotli-dev libminizip-dev libspandsp-dev libcap-dev
+sudo apt install libnghttp2-dev libzstd-dev xsltproc
+
+wget https://www.wireshark.org/download/src/wireshark-3.2.2.tar.xz -O /tmp/wireshark-3.2.2.tar.xz
+tar -xvf /tmp/wireshark-3.2.2.tar.xz --directory=/tmp/
+cd /tmp/wireshark-3.2.2
+sudo apt update && sudo apt dist-upgrade
+sudo apt install cmake libglib2.0-dev libgcrypt20-dev flex yacc bison byacc \
+  libpcap-dev qtbase5-dev libssh-dev libsystemd-dev qtmultimedia5-dev \
+  libqt5svg5-dev qttools5-dev
+cmake .
+make
+sudo make install
