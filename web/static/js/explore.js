22f4452dfd3dc7cea052176fead2fbd1bb15bb5a
Chakshu Gupta <chakshugupta@gmail.com>
2020-07-02 01:48:52 +0200

    Make explore.js modular
    Fix emptying table on refresh
    Add fonts used by material-table

--- web/static/js/explore.js
+++ web/static/js/explore.js
@@ -22,97 +22,80 @@ function load_module_options(){
     });
 }
 
+function call_api(api_endpoint, columns_def){
+  $(document).ready(function() {
+
+    var table = $('#datatable').dataTable({
+      ajax: {
+            type: "GET",
+            url: api_endpoint,
+            contentType:'application/json; charset=utf-8',
+            dataType: "json",
+            dataSrc: ""
+        },
+      autoWidth: true,
+      columns:columns_def,
+      destroy: true,
+      sort: true,
+      info: true,
+      paging: true,
+      oLanguage: {
+        sStripClasses: "",
+        sSearch: "",
+        sSearchPlaceholder: "Search filter...",
+        sInfo: "_START_ -_END_ of _TOTAL_",
+        sLengthMenu: '<span>Rows per page:</span><select class="browser-default">' +
+          '<option value="10">10</option>' +
+          '<option value="20">20</option>' +
+          '<option value="30">30</option>' +
+          '<option value="40">40</option>' +
+          '<option value="50">50</option>' +
+          '<option value="-1">All</option>' +
+          '</select></div>'
+      },
+      searching:true,
+      responsive: true
+    });
+  });
+}
+
 
 function load_data(event_type) {
   if ( $.fn.dataTable.isDataTable( '#datatable' ) ) {
     $('#datatable').DataTable().clear().destroy();
+    $('#datatable').empty();
   }
-
+  var columns = []
+  var api_endpoint = ""
      
   if(event_type == "honeypot-event"){
-    $(document).ready(function() {
-
-      $('#datatable').dataTable({
-        ajax: {
-              type: "GET",
-              url: "/api/events/honeypot-events",
-              contentType:'application/json; charset=utf-8',
-              dataType: "json",
-              dataSrc: ""
-          },
-        columns:[
-              { data: 'ip_src', defaultContent: '', title: "Src IP"},
-              { data: 'port_src', defaultContent: '', title: "Src Port"},
-              { data: 'ip_dest', defaultContent: '', title: "Dest IP"},
-              { data: 'port_dest', defaultContent: '', title: "Dest Port"},
-              { data: 'module_name', defaultContent: '', title: "Module Name"},
-              { data: 'date', defaultContent: '', title: "Date"},
-              { data: 'machine_name', defaultContent: '', title: "Machine Name"},
-              { data: 'country_ip_src', defaultContent: '', title: "Country: Src IP"},
-              { data: 'country_ip_dest', defaultContent: '', title: "Country: Dest IP"}],
-        bSort: true,
-        info: true,
-        paging: true,
-        oLanguage: {
-          sStripClasses: "",
-          sSearch: "",
-          sSearchPlaceholder: "Search filter...",
-          sInfo: "_START_ -_END_ of _TOTAL_",
-          sLengthMenu: '<span>Rows per page:</span><select class="browser-default">' +
-            '<option value="10">10</option>' +
-            '<option value="20">20</option>' +
-            '<option value="30">30</option>' +
-            '<option value="40">40</option>' +
-            '<option value="50">50</option>' +
-            '<option value="-1">All</option>' +
-            '</select></div>'
-        },
-        bAutoWidth: true,
-        searching:true
-      });
-    });
-
+    columns = [
+      { data: 'ip_src', defaultContent: '', title: "Src IP"},
+      { data: 'port_src', defaultContent: '', title: "Src Port"},
+      { data: 'ip_dest', defaultContent: '', title: "Dest IP"},
+      { data: 'port_dest', defaultContent: '', title: "Dest Port"},
+      { data: 'module_name', defaultContent: '', title: "Module Name"},
+      { data: 'date', defaultContent: '', title: "Date"},
+      { data: 'machine_name', defaultContent: '', title: "Machine Name"},
+      { data: 'country_ip_src', defaultContent: '', title: "Src Country"},
+      { data: 'country_ip_dest', defaultContent: '', title: "Dest Country"}];
+    
+    api_endpoint = "/api/events/honeypot-events";
+    call_api(api_endpoint, columns);
   }
   else if( event_type == "network-event"){
-      $(document).ready(function() {
-          $('#datatable').dataTable({
-            ajax: {
-                  type: "GET",
-                  url: "/api/events/network-events",
-                  contentType:'application/json; charset=utf-8',
-                  dataType: "json",
-                  dataSrc: ""
-              },
-            columns:[
-                  { data: 'ip_src', defaultContent: '', title: "Src IP"},
-                  { data: 'port_src', defaultContent: '', title: "Src Port"},
-                  { data: 'ip_dest', defaultContent: '', title: "Dest IP"},
-                  { data: 'port_dest', defaultContent: '', title: "Dest Port"},
-                  { data: 'date', defaultContent: '', title: "Date"},
-                  { data: 'machine_name', defaultContent: '', title: "Machine Name"},
-                  { data: 'country_ip_src', defaultContent: '', title: "Src Country"},
-                  { data: 'country_ip_dest', defaultContent: '', title: "Dest Country"}],
-            bSort: true,
-            info: true,
-            paging: true,
-            oLanguage: {
-              sStripClasses: "",
-              sSearch: "",
-              sSearchPlaceholder: "Search filter...",
-              sInfo: "_START_ -_END_ of _TOTAL_",
-              sLengthMenu: '<span>Rows per page:</span><select class="browser-default">' +
-                '<option value="10">10</option>' +
-                '<option value="20">20</option>' +
-                '<option value="30">30</option>' +
-                '<option value="40">40</option>' +
-                '<option value="50">50</option>' +
-                '<option value="-1">All</option>' +
-                '</select></div>'
-            },
-            bAutoWidth: true,
-            searching: true
-          });
-        });
+    columns = [
+      { data: 'ip_src', defaultContent: '', title: "Src IP"},
+      { data: 'port_src', defaultContent: '', title: "Src Port"},
+      { data: 'ip_dest', defaultContent: '', title: "Dest IP"},
+      { data: 'port_dest', defaultContent: '', title: "Dest Port"},
+      { data: 'date', defaultContent: '', title: "Date"},
+      { data: 'machine_name', defaultContent: '', title: "Machine Name"},
+      { data: 'country_ip_src', defaultContent: '', title: "Src Country"},
+      { data: 'country_ip_dest', defaultContent: '', title: "Dest Country"}];
+
+    api_endpoint = "/api/events/network-events";
+    call_api(api_endpoint, columns);
   }
 }
 
