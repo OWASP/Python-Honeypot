cb46ec7962f90f66dd72109de2f1de0fc12260b5
dhirensr <dhirensr@gmail.com>
2020-07-23 17:25:46 +0200

    added logging functions in core.log
    what: 1. core.log was missing and hence I added the log functionality.

---
+++ core/log.py
@@ -0,0 +1,35 @@
+import json
+from core.compatible import version
+
+def __log_into_file(filename, mode, data):
+    """
+    write a content into a file (support unicode).
+    Args:
+        filename: the filename
+        mode: writing mode (a, ab, w, wb, etc.)
+        data: content
+        language: language
+    Returns:
+        True if success otherwise None
+    """
+    log = ''
+    if version() is 2:
+        if isinstance(data, str):
+            try:
+                log = json.loads(data)
+            except ValueError:
+                log = ''
+
+        with open(filename, mode) as save:
+            save.write(data + '\n')
+    else:
+        if isinstance(data, str):
+            try:
+                log = json.loads(data)
+            except ValueError:
+                log = ''
+
+
+        with open(filename, mode, encoding='utf-8') as save:
+            save.write(data + '\n')
+    return True
