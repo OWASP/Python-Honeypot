2c60350797564cd53be39929549e29c7bafd6e64
Ali Razmjoo <ali.razmjoo@owasp.org>
2020-08-07 18:25:22 +0200

    grant write permission in docker

--- modules/ftp/weak_password/__init__.py
+++ modules/ftp/weak_password/__init__.py
@@ -60,6 +60,8 @@ def module_configuration():
     return {
         "username": "admin",
         "password": "admin",
-        "extra_docker_options": ["--volume {0}/tmp/ohp_ftp_weak_container/:/root".format(os.getcwd())],
+        "extra_docker_options": [
+            "--mount \"type=bind,source={0}/tmp/ohp_ftp_weak_container,target=/root\":z".format(os.getcwd())
+        ],
         "module_processor": ModuleProcessor()
     }
--- modules/ssh/weak_password/__init__.py
+++ modules/ssh/weak_password/__init__.py
@@ -60,6 +60,8 @@ def module_configuration():
     return {
         "username": "root",
         "password": "123456",
-        "extra_docker_options": ["--volume {0}/tmp/ohp_ssh_weak_container:/root".format(os.getcwd())],
+        "extra_docker_options": [
+            "--mount \"type=bind,source={0}/tmp/ohp_ssh_weak_container,target=/root\":z".format(os.getcwd())
+        ],
         "module_processor": ModuleProcessor()
     }
