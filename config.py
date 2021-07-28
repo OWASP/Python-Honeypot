fc892594ebc5ae93153d7c3aa8f2488e441c6168
Ali Razmjoo <ali.razmjoo@owasp.org>
2021-07-29 00:17:09 +0200

    revert to english

--- config.py
+++ config.py
@@ -100,7 +100,7 @@ def user_configuration():
         JSON/Dict user configuration
     """
     return {
-        "language": "ru_RU",
+        "language": "en_US",
         "events_log_file": "tmp/ohp.log",
         "default_selected_modules": "all",  # or select one or multiple (e.g. ftp/strong_password,ssh/strong_password)
         "default_excluded_modules": None  # or any module name separated with comma
