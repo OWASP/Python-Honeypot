da18fcba6777173c2893e7cb04b8aaef3b949d16
mrigankanand <mrigank.rkee@rediffmail.com>
2022-08-02 10:13:52 +0530

    fix - kill network capture process if any container is killed

--- core/load.py
+++ core/load.py
@@ -362,13 +362,16 @@ def wait_until_interrupt(virtual_machine_container_reset_factory_time_seconds, c
                 # start containers based on selected modules
                 start_containers(configuration)
             if not new_network_events_thread.is_alive():
-                return error(messages["interrupt_application"])
+                error(messages["interrupt_application"])
+                return True
+
             if containers_are_unhealthy(configuration):
-                return error(
+                error(
                     "Interrupting the application because \"{0}\" container(s) is(are) not alive!".format(
                         ", ".join(containers_are_unhealthy(configuration))
                     )
                 )
+                return True
             if run_as_test:
                 break
         except KeyboardInterrupt:
@@ -884,6 +887,9 @@ def load_honeypot_engine():
     )
     # killed the network traffic capture process by ctrl + c... waiting to end.
     info(messages["killing_capture_process"])
+    if exit_flag:
+        # Terminate the network capture process
+        network_traffic_capture_process.terminate()
     if run_as_test:
         network_traffic_capture_process.terminate()
     # without ci it will be terminate after a few seconds, it needs to kill the tshark and update pcap file collection
